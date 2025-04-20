f96bca0bcf66691e8b69d33e1c329c23f873fd35
Soham Prajapati <sohamprajapati0511@gmail.com>
2025-04-20 23:31:39 +0200

    GATConv Troubleshooting

--- src/model/graph_model.py
+++ src/model/graph_model.py
@@ -5,6 +5,7 @@ import jax.numpy as jnp
 import torch 
 import jax 
 from jax.typing import ArrayLike
+from collections import defaultdict
 
 class GATConvJax(nnx.Module): 
     def __init__(self, 
@@ -13,16 +14,118 @@ class GATConvJax(nnx.Module):
                  heads: int, 
                  edge_dim: int, 
                  rngs: nnx.Rngs):
-        return None
+        super().__init__()
+        self.out_channels = out_channels
+        self.in_channels = in_channels
+        self.heads = heads
+        
+        rngs = rngs.spawn(['W', 'W_e', 'a_s', 'a_t', 'a_e', 'b_GAT'])
+
+        
+
+        # shared parameter matrices (no bias) 
+        self.W = nnx.Param(jax.random.normal(rngs['W'], (heads, in_channels, out_channels)))
+        
+        self.W_e = nnx.Param(jax.random.normal(rngs['W_e'], (heads, edge_dim, out_channels)))
+        
+        self.a_s = nnx.Param(jax.random.normal(rngs['a_s'], (heads, out_channels)))
+        
+        self.a_t = nnx.Param(jax.random.normal(rngs['a_t'], (heads, out_channels)))
+        
+        self.a_e = nnx.Param(jax.random.normal(rngs['a_e'], (heads, out_channels,)))
+        
+        self.b_GAT = nnx.Param(jax.random.normal(rngs['b_GAT'], (heads, out_channels,)))
 
     def __call__(self, 
-                 x: ArrayLike,
-                 jnp_edge_index: ArrayLike, 
-                 edge_attr: ArrayLike,
-                 ):
+                 x: ArrayLike, # shape: [num_nodes, in_channeles]
+                 edge_index: ArrayLike, # shape: [2, num_edges]
+                 edge_attr: ArrayLike, # shape: [num_edges, edge_feat_dim]
+                 ) -> jnp.ndarray:
         
-        raise NotImplementedError 
-        return None
+        N = x.shape[0] # num_nodes
+        E = edge_index.shape[1] # num_edges 
+        H = self.heads 
+        C_out = self.out_channels
+
+        
+        row, col = edge_index  # row: sources, col: targets
+        uq_src, uq_tgt = jnp.unique(row), jnp.unique(col)
+
+        x_src = x[uq_src]  # shape: [E, in_channels]
+        x_tgt = x[uq_tgt]  # shape: [E, in_channels]
+        e_attr = edge_attr  # shape: [E, edge_dim]
+
+        x_src_proj = jnp.einsum('hio,si->hso', self.W, x_src)   # [H, E, C_out]
+        x_tgt_proj = jnp.einsum('hio,ti->hto', self.W, x_tgt)   # [H, E, C_out]
+        e_proj = jnp.einsum('hdo,ed->heo', self.W_e, e_attr)    # [H, E, C_out]
+
+        ax_src_proj = jnp.einsum('ho,hso->hs', self.a_s, x_src_proj)
+        ax_tgt_proj = jnp.einsum('ho,hto->ht', self.a_t, x_tgt_proj)
+        ae_proj = jnp.einsum('ho,heo->he', self.a_e, e_proj)
+
+        
+
+
+
+        ############## LESS EFFICIENT ###################
+
+        # num_nodes = x.shape[0]
+
+        # edge_tuples = [(u.item(),v.item()) for u, v in edge_index.T] + [(v.item(),u.item()) for u, v in edge_index.T]
+        
+        # # attention coefficient for each edge: { (u,v): $\alpha_{u,v}$ }
+        # attention_coefficients = {edge: [] for edge in edge_tuples}
+
+        # # edge attribute dict: {(u,v): edge_attr[index corresponding to (u,v) edge]}
+        # edge_attr_dict = {edge: edge_attr[i] for i, edge in enumerate(edge_tuples)}
+
+        # # neighbor dict: {node: [list of all neighbors]}
+        # # for example: 
+        # # {1: [0, 2, 5],
+        # # 2: [1, 6],
+        # # 3: [0, 4, 7],
+        # # 4: [3, 8],
+        # # 0: [1, 3],
+        # # 5: [1],
+        # # 6: [2],
+        # # 7: [3],
+        # # 8: [4]}
+
+        # neighbor_dict = defaultdict(set)
+        # for src, tgt in edge_index.T: 
+        #     neighbor_dict[src.item()].add(tgt.item())
+        #     neighbor_dict[src.item()].add(tgt.item())
+        # neighbor_dict = {node: sorted(list(neighbors)) for node, neighbors in neighbor_dict.items()}
+
+        # # since attention coefficients use softmax with respect to other neighbors 
+        # attention_dict = {node: jnp.zeros(len(neighbor_dict[node])) for node in neighbor_dict.keys()}
+        # # >> {1: [0.,0.,0.], 2: [0., 0.]
+
+
+        # for src in range(num_nodes):
+        #     x_i = x[src]
+        #     for i, neighbor in enumerate(neighbor_dict[src]): # e.g. src = 1 has [0,2,5]
+        #         x_j = x[neighbor]
+        #         x1_ij = edge_attr_dict[(src, neighbor)]
+
+        #         # since GATConv uses LeakyRelu
+        #         attention_dict[src] = attention_dict[src].at[i].set(nnx.leaky_relu(jnp.matmul(self.a_s.T, jnp.matmul(self.W, x_i)) + 
+        #                                                   jnp.matmul(self.a_t.T, jnp.matmul(self.W, x_j)) + 
+        #                                                   jnp.matmul(self.a_e.T, jnp.matmul(self.W_e, x1_ij))))
+        #     attention_dict[src] = nnx.softmax(jnp.array(attention_dict[src]))
+
+        #     for i, neighbor in enumerate(neighbor_dict[src]): 
+        #         attention_coefficients[(src, neighbor)] = attention_dict[src][i]
+        
+        # # making the attention adjacency matrix 
+        # attention_adjacency = jnp.zeros((num_nodes, num_nodes))
+        # for u, v in attention_coefficients.keys(): 
+        #     attention_adjacency = attention_adjacency.at[u, v].set(attention_coefficients[(u, v)])
+        
+        # # x_gat_output 
+        # x_gat_output = jnp.matmul(attention_adjacency, jnp.matmul(x, self.W.T))
+
+        # return x_gat_output
 
 
 class NEGATRegressorJAX(nnx.Module):
@@ -45,6 +148,8 @@ class NEGATRegressorJAX(nnx.Module):
         self.bias = bias 
         self.gnn_param_matrix = []
 
+        
+
         ############ GNN: node regression convolution layers #################
         self.node_layers = dict()
 
@@ -52,6 +157,9 @@ class NEGATRegressorJAX(nnx.Module):
 
         if len(list_node_hidden_features) != 0: 
             for idx, hid_feats_n in enumerate(list_node_hidden_features): 
+                self.node_layers[idx] = [nnx.Param(jax.random.normal())]
+
+
                 self.node_layers[idx] = [nnx.Linear(in_features=in_feats_n, 
                                                   out_features=hid_feats_n, 
                                                   rngs=rngs) for _ in range(k_hop_node)]
@@ -94,14 +202,6 @@ class NEGATRegressorJAX(nnx.Module):
                                       heads=gat_head, 
                                       edge_dim=edge_output_features)
 
-
-
-
-
-
-
-
-
     def __call__(self, 
                  tupleData: Tuple[Data]) -> jnp.array:
         
@@ -112,6 +212,9 @@ class NEGATRegressorJAX(nnx.Module):
         x1 = jnp.array(edge_data.x.detach().cpu().numpy())
         L_1l = self.jax_adjacency_matrix(edge_data.edge_index, edge_data.edge_attrs)
         L_1u = self.jax_adjacency_matrix(edge_data.edge_index_u, edge_data.edge_attrs2)
+
+        ############ gnn based node-regression ############### 
+
 
 
 
